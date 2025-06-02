2142256138f6fc5f448f80fa263b0f8edfa603bc
BirykovRV <r.v.birukov@yandex.ru>
2025-06-02 20:11:41 +0300

    implementing handlers for api

---
+++ cmd/api/movies.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "create new movie")
+}
+
+func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
+	movieId := r.PathValue("id")
+
+	id, err := strconv.ParseInt(movieId, 10, 64)
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Otherwise, interpolate the movie ID in a placeholder response.
+	fmt.Fprintf(w, "show the details of movie %d\n", id)
+}
---
+++ cmd/api/routes.go
@@ -0,0 +1,13 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() http.Handler {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
+	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
+	mux.HandleFunc("/v1/movies/:id", app.showMovieHandler)
+
+	return mux
+}
