59159880ff8589d0a0a127aed3fe814ccc764626
BirykovRV <r.v.birukov@yandex.ru>
2025-06-06 21:23:47 +0300

    Add badRequestResponse helper and update createMovieHandler for error handling

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -20,7 +20,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 	}
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
-		app.errorResponse(w, r, http.StatusBadRequest, err.Error())
+		app.badRequestResponse(w, r, err)
 		return
 	}
 
