e91e0bc964835a43083072af535f22000c6d7e40
BirykovRV <r.v.birukov@yandex.ru>
2025-06-06 15:56:23 +0300

    Add error handling and JSON response helpers for API

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
+
+	"github.com/BirykovRV/urban-train/internal/data"
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Request) {
@@ -19,6 +22,18 @@ func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request)
 		return
 	}
 
-	// Otherwise, interpolate the movie ID in a placeholder response.
-	fmt.Fprintf(w, "show the details of movie %d\n", id)
+	movie := data.Movie{
+		ID:        id,
+		CreatedAt: time.Now(),
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 }
--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -7,7 +7,7 @@ func (app *application) routes() http.Handler {
 
 	mux.HandleFunc("/v1/healthcheck", app.healthcheckHandler)
 	mux.HandleFunc("POST /v1/movies", app.createMovieHandler)
-	mux.HandleFunc("/v1/movies/:id", app.showMovieHandler)
+	mux.HandleFunc("/v1/movies/{id}", app.showMovieHandler)
 
 	return mux
 }
